98556387fcacd30123ef51d0fc21c9db529f9ee2
gabriellengoo <gabriellengoo@hotmail.com>
2021-01-19 21:02:08 +0000

    yaya

---
+++ ual.project/assets/stuff.js
@@ -0,0 +1,14 @@
+
+
+
+// function setup(){
+//     let canvas = createCanvas(200,200);
+//     canvas.parent('p5Canvas');
+//   }
+  
+//   function draw() {
+//     background(mouseX - 100,mouseY- 100 ,200);
+//   }
+
+   
+
---
+++ ual.project/assets2/stuffbox.js
@@ -0,0 +1,11 @@
+
+function setup(){
+    let canvas = createCanvas(200,200);
+    canvas.parent('p5Canvas');
+  }
+  
+  function draw() {
+    background(pmouseX,pmouseY, 56);
+  }
+
+   
