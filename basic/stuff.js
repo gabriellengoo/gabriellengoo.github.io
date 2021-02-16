851503f7bacaab999ed3ae6632242a2f8717c7c7
gabriellengoo <gabriellengoo@hotmail.com>
2021-02-16 04:18:51 +0000

    update

--- basic/stuff.js
+++ basic/stuff.js
@@ -25,37 +25,75 @@
 
 
 // ball styles
-let ball = document.body.appendChild(document.createElement('div'));
-Object.assign(ball.style, {
-  position: 'absolute',
-  background: 'rgb(47,47,47)', 
-  width: '40px', height: '40px', 
-  borderRadius: '50%'
+// let ball = document.body.appendChild(document.createElement('div'));
+// Object.assign(ball.style, {
+//   position: 'absolute',
+//   background: 'rgb(47,47,47)', 
+//   width: '20px', height: '20px', 
+//   borderRadius: '50%'
+// });
+
+// let x = -100;
+// let y = window.innerHeight / 5;
+// let vx = 3;
+// let vy = -3;
+// let grav = .3;
+// var decay = 0.5;
+
+// function loop() {
+//   let floor = window.innerHeight - 420;
+//   x += vx;
+//   y += vy;
+  
+//   vy += grav;
+//   if (y > floor) {
+//     vy *= -decay;
+//     vx *= decay;
+//     y = floor;
+//   }
+  
+//   ball.style.transform = `translate(${x}px, ${y}px)`;
+//   window.requestAnimationFrame(loop);
+// }
+// loop();
+
+
+$(document).ready(function(){
+  $(".me").click(function(){
+    $("about_me/index.html").slideUp();
+  });
+  // $(".btn2").click(function(){
+  //   $("p").slideDown();
+  // });
 });
 
-let x = -100;
-let y = window.innerHeight / 5;
-let vx = 3;
-let vy = -3;
-let grav = .3;
-var decay = 0.5;
 
-function loop() {
-  let floor = window.innerHeight - 420;
-  x += vx;
-  y += vy;
+/* Linear gradient------------------------------------------  */
+$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
   
-  vy += grav;
-  if (y > floor) {
-    vy *= -decay;
-    vx *= decay;
-    y = floor;
-  }
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
   
-  ball.style.transform = `translate(${x}px, ${y}px)`;
-  window.requestAnimationFrame(loop);
-}
-loop();
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #deffe5, rgb(255,255,255))');
+});
 
 
+/* Blur------------------------------------------  */
+$(document).ready(function(){
+  $('.vertical-menu a').hover(function(){
+      $(this).addClass('blur');
+  }).mouseout(function(){
+      $(this).removeClass('blur');
+  });
+});
+
+$(document).ready(function(){
+  $('.navbarbottom a').hover(function(){
+      $(this).addClass('blur');
+  }).mouseout(function(){
+      $(this).removeClass('blur');
+  });
+});
 
